fe9a32792d99d192072a2d55b19549a42f7f371b
Raouf Madani <raoufmadani@live.fr>
2020-03-02 21:25:41 +0100

    Complete ImageBackground

--- screens/homeScreen.jsx
+++ screens/homeScreen.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, ImageBackground, View, ScrollView} from 'react-native';
 import { PricingCard } from 'react-native-elements';
 
 const HomeScreen = props =>{
   return (
     <View style={styles.container}>
-      <PricingCard
-  color="#4f9deb"
-  title="Free"
-  price="$0"
-  info={['1 User', 'Basic Support', 'All Core Features']}
-  button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
-/>
+      <ImageBackground source={require('../assets/images/backgroundFoot.jpg')} style={styles.bigBackgroundImage}>
+        <ScrollView>
+          <PricingCard
+            color="#4f9deb"
+            title="Free"
+            price="$0"
+            info={['1 User', 'Basic Support', 'All Core Features']}
+            button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+          />
+          <PricingCard
+            color="#4f9deb"
+            title="Free"
+            price="$0"
+            info={['1 User', 'Basic Support', 'All Core Features']}
+            button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+          />
+        </ScrollView>
+      </ImageBackground>
     </View>
   );  
 };
@@ -23,6 +34,13 @@ const styles= StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  bigBackgroundImage:{
+    flex:1,
+    resizeMode:'cover',
+    height:'100%',
+    width:'100%'
   }
   
 });
