1b66e8411037b7819006139b06b3ff2ffb85cc28
El Hareth Snoussi <hareth.snoussi@gmail.com>
2020-03-02 16:21:11 +0100

    Pricing Card Testing

--- screens/homeScreen.jsx
+++ screens/homeScreen.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import { PricingCard } from 'react-native-elements';
 
 const HomeScreen = props =>{
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <PricingCard
+  color="#4f9deb"
+  title="Free"
+  price="$0"
+  info={['1 User', 'Basic Support', 'All Core Features']}
+  button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+/>
     </View>
   );  
 };
