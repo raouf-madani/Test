68207633d711dbc5ac0b8b196366a4accf6235d7
Raouf Madani <raoufmadani@live.fr>
2020-03-09 11:16:58 +0100

    Update userServiceScreen

--- screens/user/userServiceScreen.jsx
+++ screens/user/userServiceScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet} from 'react-native';
+import { StyleSheet,ImageBackground, ScrollView,View} from 'react-native';
 //import {Button} from 'react-native-paper';
 import Colors from '../../constants/Colors';
 import {Ionicons} from '@expo/vector-icons';
@@ -12,16 +12,44 @@ const UserServiceScreen = props =>{
 
    
     return(
-          
-                <ServiceCard 
-                    serviceNumber={1}
-                    typeMatch='5 vs 5'
-                    durationMatch={1}
-                    daysMatch='Sam - Ven'
-                    timeMatch='9h - 22h'
-                    price={3500}
-                />
-              
+           
+           <ImageBackground source={require('../../assets/images/stadium.jpg')} style={styles.stadiumImageBackground} blurRadius={0}>
+            <ScrollView>
+            <ServiceCard 
+                serviceNumber={1}
+                typeMatch='5 vs 5'
+                durationMatch={1}
+                daysMatch='Sam - Ven'
+                timeMatch='9h - 22h'
+                price={3500}
+            />
+            <ServiceCard 
+                serviceNumber={2}
+                typeMatch='11 vs 11'
+                durationMatch={1}
+                daysMatch='Sam - Ven'
+                timeMatch='9h - 22h'
+                price={3500}
+            />
+            <ServiceCard 
+                serviceNumber={3}
+                typeMatch='5 vs 5'
+                durationMatch={2}
+                daysMatch='Sam - Mar'
+                timeMatch='18h - 22h'
+                price={3500}
+            />
+            <ServiceCard 
+                serviceNumber={4}
+                typeMatch='10 vs 10'
+                durationMatch={2}
+                daysMatch='Sam - Ven'
+                timeMatch='9h - 18h'
+                price={3500}
+            />
+            </ScrollView>
+           </ImageBackground>
+         
          
           
      );    
@@ -29,7 +57,12 @@ const UserServiceScreen = props =>{
 
 
 const styles= StyleSheet.create({
-
+stadiumImageBackground:{
+    flex:1,
+    backgroundColor:'white',
+    alignItems:'center',
+    paddingTop:100
+}
 });
 
 export default UserServiceScreen;
